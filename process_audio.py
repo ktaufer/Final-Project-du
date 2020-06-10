d9a0aebe42260548a7c6e7d99578ae424f8577e1
ktaufer <56054046+ktaufer@users.noreply.github.com>
2020-06-09 20:10:43 -0600

    new edits

--- process_audio.py
+++ process_audio.py
@@ -1,25 +1,78 @@
 # This file contains helper functions for the flask app
 
 # dependencies
+import pandas as pd
+import math
 import numpy as np
 import librosa
 import os
 from pydub import AudioSegment
 import keras.models
 from werkzeug.utils import secure_filename
-
-# load and compile model
+from tensorflow.keras.models import load_model
  
-
 # use model to predict species from audio sample, deliver predicted label
+def make_prediction(data):
+    bird_model = load_model('bird_model.h5')
+    X = data[np.newaxis, ...]
+    prediction_array = bird_model.predict(X)
+
+    predicted_index = np.argmax(prediction_array, axis=1)
+
+    species_df = pd.read_csv('bird_sightings.csv')
+
+    species_list = []
+    common_names = []
+    for row in species_df:
+        if row['Species'] in species_list:
+            pass
+        else:
+            species_list.append(row['Species'])
+        if row['English_name'] in common_names:
+            pass
+        else:
+            common_names.append(row['English_name'])
+
+    d = {'Species': species_list, 'Common Name': common_names}
+    name_df = pd.DataFrame(d)
+    name_df = name_df.sort_values('Species')
+    
+    for index, rows in name_df.iterrows():
+        if predicted_index == row[index]:
+            prediction = (f'{row['Species']}, {row['Common Name']} ')
+
+    
+    return prediction
+
+
+
+
 
 
 # load audio file, transform, extract MFCCs
 def transform_file(file):
+    sample_rate = 22050
+    n_mfcc = 26
+    n_fft = 2048
+    hop_length = 512
+    num_segments = 1
+    duration = 30
+    samples_per_track = sample_rate * duration 
+    num_samples_per_segment = int(samples_per_track / num_segments)
+    expected_vectors_per_segment = math.ceil(num_samples_per_segment / hop_length)
 
+    signal, sr = librosa.load(file, sr = sample_rate)
+    for s in range(num_segments):
+        start_sample = num_samples_per_segment * s
+        finish_sample = start_sample + num_samples_per_segment
+        mfccs = librosa.feature.mfcc(signal[start_sample:finish_sample],
+                                    sr = sample_rate, n_fft = n_fft, 
+                                    n_mfcc = n_mfcc, hop_length = hop_length)
+        mfccs = mfccs.T
+        if len(mfccs) == expected_vectors_per_segment: 
 
+            return mfccs.to_list()
 
-    return mfccs
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -49,4 +102,7 @@ def upload_file():
                 else:
                     extract = sound[0:30000]
                 wav_file = extract.export('data.wav', format = 'wav')
-                mfccs = transform_file(wav_file)
+                data = transform_file(wav_file)
+                make_prediction(data)
+
+    return prediction
